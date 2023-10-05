1e8638be32c25bac05a5eb44366ebc413fd92ff9
Oops14 <ups14q@gmail.com>
2023-10-05 14:22:53 +0300

    Added login popup.

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
+/*
+* Slider.
+*/
 const allSlides = document.querySelectorAll('.mySlides');
 const dot = document.querySelectorAll('.dot');
+
+// Sliders buttons.
 const leftBtn = document.querySelector(".prev");
 const rightBtn = document.querySelector(".next");
-
 let activeSlide = 0;
+
+/*
+* Popup.
+*/
+const popup_bg = document.querySelector('.popup-bg');
+const popupButton = document.querySelector('.header-my-account');
+const innerPopup = document.querySelector('.inner-popup');
+const registerBtn = document.querySelector('.sign-up-btn');
+const registerPopup = document.querySelector('.register-inner-popup');
 
 function setActiveSlides() {
     allSlides.forEach((el) => el.classList.remove("active"));
@@ -22,7 +35,6 @@ leftBtn.addEventListener('click', () => {
 
     setActiveSlides();
 })
-
 rightBtn.addEventListener('click', () => {
     activeSlide++;
 
@@ -34,3 +46,42 @@ rightBtn.addEventListener('click', () => {
 })
 
 setActiveSlides();
+
+/*
+* Show the popup form.
+*/
+popupButton.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    innerPopup.classList.toggle('popup-active');
+})
+
+/*
+* Show bg for the popup.
+*/
+popupButton.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    popup_bg.classList.add('popup-bg-show');
+})
+
+/*
+* Hide popup with the bg.
+*/
+popup_bg.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    popup_bg.classList.remove('popup-bg-show');
+    innerPopup.classList.remove('popup-active');
+    registerPopup.classList.remove('register-active');
+})
+
+/*
+* Register button click.
+*/
+registerBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    innerPopup.classList.remove('popup-active');
+    registerPopup.classList.add('register-active');
+})
